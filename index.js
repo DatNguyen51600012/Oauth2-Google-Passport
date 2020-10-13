2c66c77721d141487351e73732baa662f4fa97b6
DatNguyen51600012 <43365714+DatNguyen51600012@users.noreply.github.com>
2020-10-13 11:33:09 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
 
+require('./passport-setup')
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+passport.deserializeUser(function(user, done) {
+      done(null, user);
+});
+
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extend: false}))
@@ -16,11 +26,21 @@ app.use(cookieSession({
     keys: ['key1', 'key2' ]
 }))
 
+const isLoggedIn = (req,res,next) => {
+    if ( req.user){
+        next();
+    } else {
+        res.sendStatus(401)
+    }
+}
+
 app.use(passport.initialize());
+
 app.use(passport.session());
 
+app.get('/',(req , res) => res.send('welcome'))
 app.get('/failed',(req , res) => res.send('failed'))
-app.get('/good',(req,res) => res.send('welcome ${req.user.email} !'))
+app.get('/good',(req,res) => res.send('welcome ${req.user.displayName} !'))
 
 
 app.get('/google',
@@ -37,5 +57,11 @@ app.get( '/google/callback',
         res.redirect('/good');
     } 
 ));
+
+app.get('/logout',(req,res) => {
+    req.seesion = null;
+    req.logout();
+    req.redirect('/');
+})
 
 app.listen(3000, () => console.log('dasd'))
--- passport-setup.js
+++ passport-setup.js
@@ -7,12 +7,10 @@ passport.use(new GoogleStrategy({
     clientID:     '540199931576-3hjtrp7kakmsrtegji0m4o8tgpbvfh24.apps.googleusercontent.com',
     clientSecret: 'GBiZnC4RXl1eLc_6NWJ8qXO0',
     callbackURL: "http://localhost:3000/google/callback",
-    passReqToCallback   : true
+    
   },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
+  function(accessToken, refreshToken, profile, done) {    
+    return done(null, profile);    
   }
 
 ));
